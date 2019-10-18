80e894ed8bedca9c128b538b7ffe0a6fc677fe99
SergM <snmartusenko@gmail.com>
2019-10-18 16:50:57 +0300

    created model; configured DB connection; created home page and navbar

--- config/datastores.js
+++ config/datastores.js
@@ -51,7 +51,11 @@ module.exports.datastores = {
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
 
-  },
+    adapter: 'sails-mongo',
+    url: 'mongodb://root@localhost/articlebase'
+  }
+
+
 
 
 };
